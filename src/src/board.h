3a9ceba2a97edfca4e6538eea02e2dfe87f82a2c
walton.wang <waltonwang1922@gmail.com>
2019-12-02 01:36:49 -0500

    Implemented repeating tile logic

--- src/src/board.h
+++ src/src/board.h
@@ -9,19 +9,35 @@
 #define TILE_HEIGHT 8
 #define TILE_WIDTH 8
 
+#define TILE_HEIGHT_1 (TILE_HEIGHT - 1)
+#define TILE_WIDTH_1 (TILE_WIDTH - 1)
+
+#define TILE_SIZE TILE_HEIGHT * TILE_WIDTH
+
 #define GET_TILE( __tiles, __i, __j, NTILESC)  (__tiles[(__i / TILE_HEIGHT) * NTILESC + (__j / TILE_WIDTH)])
 #define TILE(__tile, __i, __j) (__tile[(__i % TILE_HEIGHT) * TILE_HEIGHT + (__j % TILE_HEIGHT)])
 
 #define _BOARD_(__tiles, __i, __j, NTILESC) (TILE(GET_TILE(__tiles, __i, __j, NTILESC).tile_0, __i, __j))
 #define _BOARD_NEXT(__tiles, __i, __j, NTILESC) (TILE(GET_TILE(__tiles, __i, __j, NTILESC).next_tile, __i, __j))
-
+#define _BOARD_2(__tiles, __i, __j, NTILESC) (TILE(GET_TILE(__tiles, __i, __j, NTILESC).tile_2, __i, __j))
 
 typedef struct {
+    char* tile_2;
+    char* tile_1;
     char* tile_0;
     char* next_tile;
     int tile_row;
     int tile_col;
     int gen;
+    char repeating;
+    char top_updated;
+    char right_updated;
+    char bot_updated;
+    char left_updated;
+    char top_left_updated;
+    char top_right_updated;
+    char bot_right_updated;
+    char bot_left_updated;
 } TileState;
 
 void swap_all_tiles(TileState* global_ts, int ntilesr, int ntilesc);
@@ -31,10 +47,11 @@ char board(TileState* ts, int i, int j, int ntilesc) {
     int b_j = j / TILE_WIDTH;
     int o_i = i % TILE_HEIGHT;
     int o_j = j % TILE_WIDTH;
+
     return ts[b_i * ntilesc + b_j].tile_0[o_i * TILE_WIDTH + o_j];
 }
+
 void print_board(char* board, int nrows, int ncols) {
-    printf("what\n");
     printf("Printing board:\n");
     for (int row = 0; row < nrows; row++) {
         for (int col = 0; col < ncols; col++) {
@@ -43,6 +60,84 @@ void print_board(char* board, int nrows, int ncols) {
         printf("\n");
     }
     printf("\n");
+}
+
+void print_board_state(TileState *ts) {
+    printf("current\n");
+    print_board(ts->tile_0, TILE_HEIGHT, TILE_WIDTH);
+    printf("prev2\n");
+    print_board(ts->tile_2, TILE_HEIGHT, TILE_WIDTH);
+
+}
+void print_surrounding_states(TileState* ts, TileState* global_ts, int ntilesc, int ntilesr) {
+
+    int i, i_start, i_end, i_gap1, i_gap2;
+    int j, j_start, j_end, j_gap1, j_gap2;
+
+    i_start = mod((ts->tile_row - 1), ntilesr) * TILE_HEIGHT;
+    i_end = mod((ts->tile_row + 2),ntilesr) * TILE_HEIGHT;
+    j_start = mod ((ts->tile_col - 1), ntilesc) * TILE_WIDTH;
+    j_end = mod((ts->tile_col + 2),ntilesc) * TILE_WIDTH;
+
+    i_gap1 = mod((ts->tile_row),ntilesr) * TILE_HEIGHT;
+    i_gap2 = mod((ts->tile_row + 1), ntilesr) * TILE_HEIGHT;
+    j_gap1 = mod((ts->tile_col), ntilesc) * TILE_WIDTH;
+    j_gap2 = mod((ts->tile_col + 1), ntilesc) * TILE_WIDTH;
+
+
+    printf("i_start %d ", i_start);
+    printf("i_end %d ", i_end);
+    printf("i_gap1 %d ", i_gap1);
+    printf("i_gap2 %d\n", i_gap2);
+
+    printf("j_start %d ", j_start);
+    printf("j_end %d ", j_end);
+    printf("j_gap1 %d ", j_gap1);
+    printf("j_gap2 %d\n", j_gap2);
+
+    i = i_start;
+    j = j_start;
+    printf("Current Tile:\n");
+    while (i != i_end) {
+        if (i == i_gap1 || i == i_gap2) {
+            printf("\n");
+        }
+        while (j != j_end) {
+            if (j == j_gap1 || j == j_gap2) {
+                printf(" ");
+            }
+            printf("%d", _BOARD_(global_ts,i, j, ntilesc));
+            j = (j + 1) % (ntilesc * TILE_HEIGHT);
+            
+        }
+        
+
+        printf("\n");
+        i = (i + 1) % (ntilesr * TILE_WIDTH);
+        j = j_start;
+    }
+
+    i = i_start;
+    j = j_start;
+    printf("\n2 tiles ago:\n");
+    while (i != i_end) {
+        if (i == i_gap1 || i == i_gap2) {
+            printf("\n");
+        }
+        while (j != j_end) {
+            if (j == j_gap1 || j == j_gap2) {
+                printf(" ");
+            }
+            printf("%d", _BOARD_2(global_ts,i, j, ntilesc));
+            j = (j + 1) % (ntilesc * TILE_HEIGHT);
+            
+        }
+        
+
+        printf("\n");
+        i = (i + 1) % (ntilesr * TILE_WIDTH);
+        j = j_start;
+    }
 }
 
 char read_board(TileState* ts, int i, int j, int ntilesc, int gen) {
@@ -66,14 +161,16 @@ swap_all_tiles(TileState* global_ts, int ntilesr, int ntilesc) {
         for (j = 0; j < ntilesc; j++) {
 
             id = i*ntilesc+j;
-            temp = global_ts[id].tile_0;
+
+            temp = global_ts[id].tile_2;
+            global_ts[id].tile_2 = global_ts[id].tile_1;
+            global_ts[id].tile_1 = global_ts[id].tile_0;
             global_ts[id].tile_0 = global_ts[id].next_tile;
             global_ts[id].next_tile = temp;
 
         }
     }
 }
-
 
 int* init_gen_board(int ntilesr, int ntilesc) {
     int* gen_tiles = (int*) malloc(ntilesr * ntilesc * sizeof(int));
@@ -93,22 +190,39 @@ TileState* init_board(char* board, int nrows, int ncols) {
     int is, ie, js, je;
     char* curr_tile;
     char* next_tile;
+    char* prev_tile;
+    char* pprev_tile;
 
     int id;
 
     for (i = 0; i < num_tiles_h; i++) {
         for (j = 0; j < num_tiles_w; j++) {
-            curr_tile = (char*) malloc(TILE_HEIGHT * TILE_HEIGHT * sizeof(char));
-            next_tile = (char*) malloc(TILE_HEIGHT * TILE_HEIGHT * sizeof(char));
+            pprev_tile = (char*) malloc(TILE_HEIGHT * TILE_WIDTH * sizeof(char));
+            prev_tile = (char*) malloc(TILE_HEIGHT * TILE_WIDTH * sizeof(char));
+            curr_tile = (char*) malloc(TILE_HEIGHT * TILE_WIDTH * sizeof(char));
+            next_tile = (char*) malloc(TILE_HEIGHT * TILE_WIDTH * sizeof(char));
+
             if (curr_tile == NULL || next_tile == NULL) {
                 return NULL;
             }
+
             id = i * num_tiles_w + j;
+            res[id].tile_2 = pprev_tile;
+            res[id].tile_1 = prev_tile;
             res[id].tile_0 = curr_tile;
             res[id].next_tile = next_tile;
             res[id].tile_row = i;
             res[id].tile_col = j;
             res[id].gen = 0;
+            res[id].repeating = 0;
+            res[id].top_updated = 0;
+            res[id].right_updated = 0;
+            res[id].bot_updated= 0;
+            res[id].left_updated= 0;
+            res[id].top_left_updated= 0;
+            res[id].top_right_updated= 0;
+            res[id].bot_right_updated= 0;
+            res[id].bot_left_updated= 0;
 
             is = i * TILE_HEIGHT;
             ie = is + TILE_HEIGHT;
--- src/src/life.c
+++ src/src/life.c
@@ -11,12 +11,13 @@
 
 #include <stdio.h>
 #include <pthread.h>
+#include <assert.h>
 /*****************************************************************************
  * Helper function definitions
  ****************************************************************************/
 
 #define NUM_THREADS 16
-
+#define GEN_THRESH 5
 
 // Global variables
 pthread_mutex_t global_lock = PTHREAD_MUTEX_INITIALIZER;
@@ -34,9 +35,91 @@ int ntilesr;
 int max_gens;
 int max_gens_1;
 
+int total_tiles = 0;
+int repeating_tile = 0;
+int repeating_tile_init = 0;
+int repeating_body = 0;
+
 /*****************************************************************************
  * Game of life implementation
  ****************************************************************************/
+
+char compare_char(char* left, char* right, int len) {
+	if (memcmp(left, right, len) == 0) {
+		return 1;
+	}
+	return 0;
+}
+
+void inline
+atomic_increment(int* count) {
+	pthread_mutex_lock(&global_lock);
+	(*count)++;
+	pthread_mutex_unlock(&global_lock);
+}
+
+char top_edge_repeating(TileState* ts) {
+	char* current = ts->tile_0;
+	char* prev_2 = ts->tile_2;
+
+	if (memcmp(current, prev_2, TILE_WIDTH) == 0) {
+		// printf("Top Edge:\ncurrent:\t");
+		// for (int i = 0; i < TILE_WIDTH; i++) {
+		// 	printf("%d", current[i]);
+		// }
+		// printf("\nprev_2  :\t");
+		// for (int i = 0; i < TILE_WIDTH; i++) {
+		// 	printf("%d", prev_2[i]);
+		// } printf("\n");
+		return 1;
+	}
+
+	return 0;
+}
+
+char bot_edge_repeating(TileState* ts) {
+	char* current = ts->tile_0 + TILE_SIZE - TILE_WIDTH ;
+	char* prev_2 = ts->tile_2 + TILE_SIZE - TILE_WIDTH ;
+	if (memcmp(current, prev_2, TILE_WIDTH) == 0) return 1;
+	return 0;
+}
+
+char left_edge_repeating(TileState* ts) {
+	char* current = ts->tile_0;
+	char* prev_2 = ts->tile_2;
+
+	char* current_max = current + TILE_SIZE;
+
+	while (current < current_max) {
+		if (*current != *prev_2) return 0;
+		current += TILE_WIDTH;
+		prev_2 += TILE_WIDTH;
+	}
+	return 1;
+}
+
+char right_edge_repeating(TileState* ts) {
+	char* current = ts->tile_0 + TILE_WIDTH_1;
+	char* prev_2 = ts->tile_2 + TILE_WIDTH_1;
+
+	char* current_max = current + TILE_SIZE;
+
+	while (current < current_max) {
+		if (*current != *prev_2) return 0;
+		current += TILE_WIDTH;
+		prev_2 += TILE_WIDTH;
+	}
+
+	// if (ts->gen == 30) {
+	// 	printf("current\n");
+	// 	print_board(ts->tile_0, TILE_WIDTH, TILE_HEIGHT);
+	// 	printf("prev\n");
+	// 	print_board(ts->tile_2, TILE_WIDTH, TILE_HEIGHT);
+	// }
+	return 1;
+}
+
+
 
 void swap_wq() {
 	WorkQueue* temp = wq;
@@ -44,37 +127,207 @@ void swap_wq() {
 	nwq = temp;
 }
 
-void update_tile(TileState* ts) {
+char surrounding_edge_repeating(TileState* ts) {
+	//printf("Starting\n");
+	int i = ts->tile_row;
+	int j = ts->tile_col;
+
+	int inorth = mod (i-1, ntilesr);
+	int isouth = mod (i+1, ntilesr);
+	int jwest = mod (j-1, ntilesc);
+	int jeast = mod (j+1, ntilesc);
+
+	TileState* top = global_ts + inorth * ntilesc + j;
+	TileState* right = global_ts + i * ntilesc + jeast;
+	TileState* bot = global_ts + isouth * ntilesc + j;
+	TileState* left = global_ts + i * ntilesc + jwest;
+
+	TileState* top_left = global_ts + inorth * ntilesc + jwest;
+	TileState* top_right = global_ts + inorth * ntilesc + jeast;
+	TileState* bot_left = global_ts + isouth * ntilesc + jwest;
+	TileState* bot_right = global_ts + isouth * ntilesc + jeast;
+	
+	int top_left_id = TILE_SIZE - 1;
+	int top_right_id = TILE_HEIGHT_1 * TILE_WIDTH;
+	int bot_left_id = TILE_WIDTH_1;
+	int bot_right_id = 0;
+	
+	char top_left_repeating = (top_left->tile_0[top_left_id] == top_left->tile_2[top_left_id]);
+	char top_right_repeating = (top_right->tile_0[top_right_id] == top_right->tile_2[top_right_id]);
+	char bot_left_repeating = (bot_left->tile_0[bot_left_id] == bot_left->tile_2[bot_left_id]);
+	char bot_right_repeating = (bot_right->tile_0[bot_right_id] == bot_right->tile_2[bot_right_id]);
+	//printf("ya\n");
+	if (top_edge_repeating(bot) && bot_edge_repeating(top)
+			&& left_edge_repeating(right) && right_edge_repeating(left)
+			&& top_left_repeating && top_right_repeating 
+			&& bot_left_repeating && bot_right_repeating) {
+		
+		// printf("Row/Col\n");
+		// printf("(%d, %d) (%d, %d), (%d, %d)\n", top_left->tile_row, top_left->tile_col,
+		// 		top->tile_row, top->tile_col, top_right->tile_row, top_right->tile_col);
+
+		// printf("(%d, %d) (%d, %d), (%d, %d)\n", left->tile_row, left->tile_col,
+		// 		ts->tile_row, ts->tile_col, right->tile_row, right->tile_col);
+
+		// printf("(%d, %d) (%d, %d), (%d, %d)\n", bot_left->tile_row, bot_left->tile_col,
+		// 		bot->tile_row, bot->tile_col, bot_right->tile_row, bot_right->tile_col);	
+		// print_surrounding_states(ts, global_ts, ntilesc, ntilesr);
+		// printf("Ending\n");
+		return 1;
+	}
+	//printf("Ending\n\n");
+	return 0;
+}
+
+
+
+char update_tile_element(TileState* ts, int tile_i, int tile_j) {
 	int i, j;
-	int el_i, el_j;
-	int id;
+	int inorth, isouth, jwest, jeast;
+	char neighbor_count;
 	char new_val;
+	int id;
 
-	for (i = 0; i < TILE_HEIGHT; i++) {
-		for (j = 0; j < TILE_WIDTH; j++) {
+	i = ts->tile_row * TILE_HEIGHT + tile_i;
+	j = ts->tile_col * TILE_WIDTH + tile_j;
 
-			el_i = ts->tile_row * TILE_HEIGHT + i;
-			el_j = ts->tile_col * TILE_WIDTH + j;
+	inorth = mod (i-1, num_rows);
+	isouth = mod (i+1, num_rows);
+	jwest = mod (j-1, num_cols);
+	jeast = mod (j+1, num_cols);
 
-			
-			const int inorth = mod (el_i-1, num_rows);
-			const int isouth = mod (el_i+1, num_rows);
-			const int jwest = mod (el_j-1, num_cols);
-			const int jeast = mod (el_j+1, num_cols);
+	neighbor_count = 
+		board (global_ts, inorth, jwest, ntilesc) + 
+		board (global_ts, inorth, j, ntilesc) + 
+		board (global_ts, inorth, jeast, ntilesc) + 
+		board (global_ts, i, jwest, ntilesc) +
+		board (global_ts, i, jeast, ntilesc) + 
+		board (global_ts, isouth, jwest, ntilesc) +
+		board (global_ts, isouth, j, ntilesc) + 
+		board (global_ts, isouth, jeast, ntilesc);
+	
+	id = tile_i * TILE_WIDTH + tile_j;
+	new_val = alivep(neighbor_count, ts->tile_0[id]);
+	if (ts->next_tile[id] != new_val) {
+		ts->next_tile[id] = new_val;
+		return 1;
+	}
+	return 0;
+}
 
-			const char neighbor_count = 
-				board (global_ts, inorth, jwest, ntilesc) + 
-				board (global_ts, inorth, el_j, ntilesc) + 
-				board (global_ts, inorth, jeast, ntilesc) + 
-				board (global_ts, el_i, jwest, ntilesc) +
-				board (global_ts, el_i, jeast, ntilesc) + 
-				board (global_ts, isouth, jwest, ntilesc) +
-				board (global_ts, isouth, el_j, ntilesc) + 
-				board (global_ts, isouth, jeast, ntilesc);
+void update_tile_element_top_left(TileState* ts) {
+	ts->top_left_updated = update_tile_element(ts, 0, 0);
+}
 
-			id = i * TILE_WIDTH + j;
-			new_val = alivep(neighbor_count, ts->tile_0[id]);
-			ts->next_tile[id] = new_val;
+void update_tile_element_top_right(TileState* ts) {
+	ts->top_right_updated = update_tile_element(ts, 0, TILE_WIDTH_1);
+}
+
+void update_tile_element_bot_left(TileState* ts) {
+	ts->bot_left_updated = update_tile_element(ts, TILE_HEIGHT_1, 0);
+}
+
+void update_tile_element_bot_right(TileState* ts) {
+	ts->bot_right_updated = update_tile_element(ts, TILE_HEIGHT_1, TILE_WIDTH_1);
+}
+
+void update_tile_element_top(TileState* ts) {
+	char updated = 0;
+	int i;
+	for (i = 1; i < TILE_WIDTH_1; i++) {
+		if (update_tile_element(ts, 0, i)) {
+			updated = 1;
+		}
+	}
+	ts->top_updated = updated;
+}
+
+void update_tile_element_right(TileState* ts) {
+	char updated = 0;
+	int i;
+	for (i = 1; i < TILE_HEIGHT_1; i++) {
+		if (update_tile_element(ts, i, TILE_WIDTH_1)) {
+			updated = 1;
+		}
+	}
+	ts->right_updated = updated;
+}
+
+void update_tile_element_bot(TileState* ts) {
+	char updated = 0;
+	int i;
+	for (i = 1; i < TILE_WIDTH_1; i++) {
+		if (update_tile_element(ts, TILE_HEIGHT_1, i)) {
+			updated = 1;
+		}
+	}
+	ts->bot_updated = updated;
+}
+
+void update_tile_element_left(TileState* ts) {
+	char updated = 0;
+	int i;
+	for (i = 1; i < TILE_HEIGHT_1; i++) {
+		if (update_tile_element(ts, i, 0)) {
+			updated = 1;
+		}
+	}
+	ts->left_updated = updated;
+}
+
+void update_tile_element_body(TileState* ts) {
+	int i, j;
+	for (i = 1; i < TILE_HEIGHT_1; i++) {
+		for (j = 1; j < TILE_WIDTH_1; j++) {
+			update_tile_element(ts, i, j);
+		}
+	}
+}
+void update_tile_edge(TileState* ts) {
+	update_tile_element_top_left(ts);
+	update_tile_element_top_right(ts);
+	update_tile_element_bot_left(ts);
+	update_tile_element_bot_right(ts);
+	update_tile_element_top(ts);
+	update_tile_element_left(ts);
+	update_tile_element_bot(ts);
+	update_tile_element_right(ts);
+}
+
+void update_tile(TileState* ts) {
+	update_tile_element_top_left(ts);
+	update_tile_element_top_right(ts);
+	update_tile_element_bot_left(ts);
+	update_tile_element_bot_right(ts);
+	update_tile_element_top(ts);
+	update_tile_element_left(ts);
+	update_tile_element_bot(ts);
+	update_tile_element_right(ts);
+	update_tile_element_body(ts);
+}
+
+void process_tile(TileState* ts) {
+	// update_tile(ts);
+	// return;
+	//printf("process t\n");
+	atomic_increment(&total_tiles);
+	if (!ts->repeating) {
+		if (ts->gen > GEN_THRESH && compare_char(ts->tile_0, ts->tile_2, TILE_SIZE) == 1
+				&& surrounding_edge_repeating(ts)) {
+			//printf("\t\tSurrounding repeating\n");
+			memcpy(ts->next_tile, ts->tile_1, TILE_SIZE);
+			ts->repeating = 1;
+			atomic_increment(&repeating_tile_init);
+		} else {
+			update_tile(ts);
+		}
+	} else {
+		if (!surrounding_edge_repeating(ts)) {
+			ts->repeating = 0;
+			update_tile_edge(ts);
+			atomic_increment(&repeating_body);
+		} else {
+			atomic_increment(&repeating_tile);
 		}
 	}
 }
@@ -103,7 +356,7 @@ void* work(void *args) {
 		} else {
 			// printf("Thread %d executing work tile (%d, %d), gen: %d\n", 
 			// 		thread_num, n->w->tile_row,n->w->tile_col,n->w->gen);
-			update_tile(n->w);
+			process_tile(n->w);
 			n->w->gen += 1;
 			if (n->w->gen < max_gens) {
 				enque(nwq, n);
@@ -120,7 +373,6 @@ game_of_life (char* outboard,
 	      const int ncols,
 	      const int gens_max)
 {
-	// printf("Input board\n");
 	// print_board(inboard, nrows, ncols);
 	num_rows = nrows;
 	num_cols = ncols;
@@ -133,7 +385,6 @@ game_of_life (char* outboard,
 	ntilesc = nrows / TILE_WIDTH;
 	ntilesr = nrows / TILE_HEIGHT;
 
-	//printf("ntilesc: %d ntilesr %d\n", ntilesc, ntilesr);
 	global_ts = init_board(inboard, nrows, ncols);
 
 	wq = init_queue_with_work(global_ts, ntilesr, ntilesc);
@@ -150,20 +401,9 @@ game_of_life (char* outboard,
         pthread_join(tid[i], NULL);
     }
 
-	//print_board(global_ts[0].tile_0, TILE_HEIGHT, TILE_WIDTH);
 	pthread_barrier_destroy(&bsync);
 	board_exit(global_ts, outboard, nrows, ncols);
-
-	// printf("inboard\n");
-	// print_board(inboard, nrows, ncols);
-
-	// printf("tile_0\n");
-	// print_board(global_ts[0].tile_0, TILE_HEIGHT, TILE_WIDTH);
-
-	// printf("next_tile\n");
-	// print_board(global_ts[0].next_tile, TILE_HEIGHT, TILE_WIDTH);
-
-	// printf("outboard\n");
-	// print_board(outboard, nrows, ncols);
+	printf("total_tiles: %d, repeating_tile %d\n", total_tiles, repeating_tile);
+	printf("repeating_tile_init: %d, repeating_body %d\n", repeating_tile_init, repeating_body);
 	return outboard;
 }
